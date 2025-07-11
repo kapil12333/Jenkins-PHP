e3d2be0b963edbde6de91145db130ad9f67b006a
Kapil Kumar <kapilprajapat8130@gmail.com>
2025-07-11 18:29:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ welcome! I love my india and also love ducat and
 </h1>
 
 <?php
-echo "Hello World! Ashi!!!!! i am adityaa from ducat and also from india 
+echo "Hello World!
 saasddafafda
 afafafeaf";
 ?>
